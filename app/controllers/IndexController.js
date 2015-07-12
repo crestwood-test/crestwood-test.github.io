072b7bc9b0294050843e8c344b32731e53930382
crestwood-test <tyler.haugen.stanley@crestwood.on.ca>
2015-07-12 16:21:12 -0400

    Adding angular code

---
+++ app/controllers/IndexController.js
@@ -0,0 +1,47 @@
+app.controller('IndexController', ['$scope', function($scope){
+	$scope.title = 'This is my own string';
+  $scope.promo = "15% off all childrens books.";
+
+  $scope.products = 
+  [
+    {
+      name: 'The Book of Trees',
+      price: 19,
+      pubdate: new Date('2014', '03', '08'),
+      cover: 'img/the-book-of-trees.jpg',
+      likes: 0,
+      dislikes: 0
+    },
+    {
+      name: 'Program or be Programmed',
+      price: 8,
+      pubdate: new Date('2013', '08', '01'),
+      cover: 'img/program-or-be-programmed.jpg',
+      likes: 0,
+      dislikes: 0
+    },
+    {
+      name: "Book 1",
+      price: 10,
+      pubdate: new Date('2015', '06', '01'),
+			cover: 'img/the-book-of-trees.jpg',
+      likes: 0,
+      dislikes: 0
+    },
+    {
+      name: "Book 2",
+      price: 100,
+      pubdate: new Date('2015', '06', '02'),
+      cover: 'img/program-or-be-programmed.jpg',
+      likes: 0,
+      dislikes: 0
+    }
+  ];
+  
+	$scope.plusOne = function(index){
+		$scope.products[index].likes += 1;
+	};
+  $scope.minusOne = function(index){
+  	$scope.products[index].dislikes += 1;
+  };
+}]);
