905460cb9931d8304a9ac5eab43f09a1d5ed34f2
crestwood-test <tyler.haugen.stanley@crestwood.on.ca>
2015-08-04 01:02:29 -0400

    Teacher page now pulls test dynamically from the backend based on user's login

--- app/js/controllers/TeacherIndexController.js
+++ app/js/controllers/TeacherIndexController.js
@@ -1,6 +1,6 @@
 app.controller('TeacherIndexController', ['$scope', function($scope){
 	
-	Parse.initialize("N7SiZg1sfRYhCWwPT0jc7qayEqKcvjtsj7cHzn72", "kVSHLLY9Xq3zNR3ldJcLEMI1d2jqnxaCy0Z8Ud2l");
+	// Parse.initialize("N7SiZg1sfRYhCWwPT0jc7qayEqKcvjtsj7cHzn72", "kVSHLLY9Xq3zNR3ldJcLEMI1d2jqnxaCy0Z8Ud2l");
 
 	$scope.currentUser = Parse.User.current().attributes.email;
 
@@ -10,36 +10,118 @@ app.controller('TeacherIndexController', ['$scope', function($scope){
 		window.location = "/index.html";
 	}
 
-	$scope.classes = 
-	[
-		{
-			className: "ENG101",
-			sectionName: "L0101"
-		},
-		{
-			className: "MAT223",
-			sectionName: "L0105"
+	// Need to get all of this teachers tests that they have published.
+	getTests = function(){
+		var AllTests = Parse.Object.extend("AllTests");
+		var query = new Parse.Query(AllTests);
+		var currentUser = Parse.User.current();
+		$scope.coursesWithTests = [];
+
+		// var temp = 
+		// [
+		// 	{
+		// 		courseCode: "MAT4U01",
+		// 		tests: 
+		// 		[
+		// 			{
+		// 				name: "Math essentials",
+		// 				publishedUrl: "www.google.ca",
+		// 				active: false,
+		// 				versionHistory: ["Yesterday", "Friday"],
+		// 			}
+		// 		]
+		// 	},
+		// 	{
+		// 		courseCode: "ENG4U01",
+		// 		tests:
+		// 		[
+		// 			{
+		// 				name: "Spelling 101",
+		// 				publishedUrl: "www.google.com",
+		// 				active: false,
+		// 				versionHistory: ["Today", "Tomorrow"],
+		// 			}
+		// 		]
+		// 	}
+		// ]
+		
+
+		query.equalTo("PublishingUser", currentUser);
+		query.find({
+			success: function(testObjects){
+				// This is all the tests that this user has published.
+				testObjects.forEach(function(testObject){
+					// Check if course code is in our object yet.
+					var courseIndex = courseCodeExists(testObject.attributes.CourseCode);
+
+					if (courseIndex > -1){ // course object found.
+						var courseObject = $scope.coursesWithTests[courseIndex];
+
+						var newTest = 
+						{
+							name: testObject.attributes.TestName,
+							publishedUrl: testObject.attributes.PublishedUrl,
+							active: testObject.attributes.Active,
+							createdAt: testObject.createdAt,
+							updatedAt: testObject.updatedAt,
+							id: testObject.id,
+							object: testObject
+						};
+
+						// Add the test to the list of tests for this specific courseCode
+						courseObject.tests.push(newTest);
+
+					} else { // Create a new courseCode object and store it in $scope.coursesWithTests & add the first test
+						var newCourseObject = 
+						{
+							courseCode: testObject.attributes.CourseCode,
+							tests: 
+							[{
+								name: testObject.attributes.TestName,
+								publishedUrl: testObject.attributes.PublishedUrl,
+								active: testObject.attributes.Active,
+								createdAt: testObject.createdAt,
+								updatedAt: testObject.updatedAt,
+								id: testObject.id,
+								object: testObject
+							}]
+						};
+						console.log("Course code doesn't exist");
+						$scope.coursesWithTests.push(newCourseObject);
+					}
+					$scope.$apply();
+				});
+			},
+			error: function(error){
+				console.log(error);
+			}
+
+		});
+	}
+	getTests();
+
+	// Look in $scope.coursesWithTests and see if there is a record of this test for this teacher already created
+	// Returns the index of the course object if found, or -1 if not found
+	courseCodeExists = function(courseCode){	
+		for (i = 0; i < $scope.coursesWithTests.length; i++){
+			var currentCourseObject = $scope.coursesWithTests[i];
+
+			if (currentCourseObject.courseCode == courseCode){
+				return i;
+			}
 		}
-	];
+		return -1;
+	};
 
-	$scope.availableTests = 
-	[
-		[{
-			className: "ENG101",
-			testName: "Spelling 101",
-			publishedUrl: "http://www.google.com"
-		},
-		{
-			className: "ENG101",
-			testName: "Canadian Short Stories",
-			publishedURL: "https://docs.google.com/a/crestwood.on.ca/forms/d/1Na7FSTZnsv-SG_nmEeY5wQ0UM9AsKsNKfW-sjDEOA7Q/viewform"
 
-		}],
-
-		[{
-			className: "MAT223",
-			testName: "Vectors",
-			publishedUrl: "http://www.reddit.com"
-		}]
-	];
+	$scope.toggleActiveTest = function(test){
+		
+		// Set the test to the opposite on the backend.
+		var testObject = test.object;
+		testObject.set("Active", !test.active);
+		testObject.save();
+		
+		// Set the test to the opposite locally
+		test.active = !test.active;
+	};
 }]);
